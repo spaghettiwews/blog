e2d8e9d016c6118a49a87469f4164816a2da1721
spaghettiwews <spaghettiwews@outlook.com>
2020-03-22 15:04:57 +0200

    remove circleci workflow and vscode settings

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=0e8cb394b6f9bed370387d728082f0c9f84dc405\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/spaghettiwews/blog/tree/master
